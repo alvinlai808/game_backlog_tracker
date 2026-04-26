6c57838debd3120d351feea18d0a562274857a00
Alvin Lai <alvinikaika@gmail.com>
2026-04-25 15:34:51 -1000

    add ability to specify game priority & get sorted list by priority

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ app = FastAPI()
 class Game(BaseModel):
     name: str
     completed: bool = False
+    priority: int = 0
 
-games = []
+games = [
+    Game(name="Elden Ring", priority=2),
+    Game(name="Halo", priority=0),
+    Game(name="Zelda", priority=1),
+    Game(name="Mario", priority=3),
+    Game(name="Tetris", priority=0)
+]
 
 @app.get("/")
 def root():
@@ -22,9 +29,22 @@ def create_game(game: Game):
 def list_games(limit: int = 10):
     return games[0:limit]
 
+def get_sort_value(game):
+    if game.priority == 0:
+        return 999999
+    
+    return game.priority
+
+@app.get("/games/priority", response_model=list[Game])
+def prioritize_games():
+    games.sort(key=get_sort_value)
+    return games
+
 @app.get("/games/{game_id}", response_model=Game)
 def get_game(game_id: int) -> Game:
     if game_id < len(games):
         return games[game_id]
     else:
         raise HTTPException(status_code=404, detail=f"Game {game_id} not found")
+    
+    
